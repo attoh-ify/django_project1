1b26e3c447f0d9050bebc8804b60a371d67f6fe1
attoh alexander <alexander.attoh22@gmail.com>
2022-10-28 05:53:58 +0100

    changes

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,21 +1,25 @@
+from email.policy import default
 from turtle import title
 from unittest.util import _MAX_LENGTH
 from django.db import models
+from datetime import datetime
 
 
 class Artiste(models.Model):
-    first_name = models.CharField()
-    last_name = models.CharField()
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
     age = models.IntegerField()
 
 
 class Song(models.Model):
-    title = models.CharField()
-    date_released = models.CharField()
-    likes = models.CharField()
-    artiste_id = models.CharField()
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.CharField(max_length=40)
+    artiste_id = models.CharField(max_length=40)
 
 
 class Lyric(models.Model):
-    content = models.CharField()
-    song_id = models.CharField()
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    song_id = models.CharField(max_length=40)
