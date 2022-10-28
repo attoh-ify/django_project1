5ae045b77efb4c6160ba69f687e65568865e0748
attoh alexander <alexander.attoh22@gmail.com>
2022-10-28 05:49:44 +0100

    changes

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,21 @@
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    title = models.CharField()
+    date_released = models.CharField()
+    likes = models.CharField()
+    artiste_id = models.CharField()
+
+
+class Lyric(models.Model):
+    content = models.CharField()
+    song_id = models.CharField()
