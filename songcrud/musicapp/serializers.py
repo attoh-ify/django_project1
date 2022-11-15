87b5012af86fbad7aa8eb5302b66ff298d7e92fe
attoh alexander <alexander.attoh22@gmail.com>
2022-11-15 16:08:30 +0100

    update serializers

--- songcrud/musicapp/serializers.py
+++ songcrud/musicapp/serializers.py
@@ -5,11 +5,10 @@ from .models import Artiste, Song
 class ArtisteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artiste
-        fields = ['id', 'first_name', 'last_name', 'age']  # same as '__all__'
+        fields = '__all__'  # ['id', 'first_name', 'last_name', 'age']
 
 
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ['id', 'Artiste', 'title', 'date_released',
-                  'likes', 'artiste_id']  # same as '__all__'
+        fields = '__all__'
