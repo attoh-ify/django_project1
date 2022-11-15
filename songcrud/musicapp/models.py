9ab5c16c2e1d5d18dd5ec7fe17fe50dc427b4b0a
attoh alexander <alexander.attoh22@gmail.com>
2022-11-15 16:05:43 +0100

    correct song and lyric model code

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -15,20 +15,15 @@ class Artiste(models.Model):
 
 
 class Song(models.Model):
-    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     date_released = models.DateField(default=datetime.today)
-    likes = models.CharField(max_length=40)
-    artiste_id = models.CharField(max_length=40)
+    likes = models.IntegerField()
 
     def __str__(self):
         return self.title + "_( by " + self.Artiste.first_name + "_" + self.Artiste.last_name + ")"
 
 
 class Lyric(models.Model):
-    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
-    song_id = models.CharField(max_length=40)
-
-    def __str__(self):
-        return self.song_id + " (" + self.Song.Artiste.first_name + "_" + self.Song.Artiste.last_name + "/" + self.Song.title + ")"
